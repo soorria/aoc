000000f8199be341436f73de4979eae60896bae0
Soorria Saruva <soorria.ss@gmail.com>
2025-12-08 20:23:59 +1100

    chore: redo runner again

--- 2025/template/main.exs
+++ 2025/template/main.exs
@@ -1,51 +1,35 @@
 defmodule Solution do
-  @input File.read!("./input")
-  @sample File.read!("./sample")
-
-  def get_input() do
-    @input
-  end
-
-  def get_sample() do
-    @sample
-  end
-
-  def run(argv) do
-    case argv do
-      ["1", "sample"] ->
-        IO.inspect({"1", "sample", Solution.part1(Solution.get_sample())})
-
-      ["2", "sample"] ->
-        IO.inspect({"2", "sample", Solution.part2(Solution.get_sample())})
-
-      ["1"] ->
-        IO.inspect({"1", "input", Solution.part1(Solution.get_input())})
-
-      ["2"] ->
-        IO.inspect({"2", "input", Solution.part2(Solution.get_input())})
-
-      ["sample"] ->
-        IO.inspect({"1", "sample", Solution.part1(Solution.get_sample())})
-        IO.inspect({"2", "sample", Solution.part2(Solution.get_sample())})
-
-      ["input"] ->
-        IO.inspect({"1", "input", Solution.part1(Solution.get_input())})
-        IO.inspect({"2", "input", Solution.part2(Solution.get_input())})
-
-      _ ->
-        IO.inspect({"1", "sample", Solution.part1(Solution.get_sample())})
-        IO.inspect({"1", "input", Solution.part1(Solution.get_input())})
-        IO.inspect({"2", "sample", Solution.part2(Solution.get_sample())})
-        IO.inspect({"2", "input", Solution.part2(Solution.get_input())})
-    end
-  end
-
   def part1(input) do
     input
   end
 
   def part2(input) do
     input
+  end
+
+  def get_input, do: File.read!("./input")
+  def get_sample, do: File.read!("./sample")
+
+  def run(argv) do
+    tasks = Map.new([{1, &part1/1}, {2, &part2/1}])
+
+    {input_type, input} =
+      case argv do
+        [_, "sample"] -> {"sample", get_sample()}
+        [_, "input"] -> {"input", get_input()}
+        [] -> {"input", get_input()}
+      end
+
+    parts =
+      case argv do
+        ["1", _] -> [1]
+        ["2", _] -> [2]
+        [] -> [1, 2]
+      end
+      |> MapSet.new()
+
+    parts
+    |> Enum.map(fn part -> IO.inspect({part, input_type, tasks[part].(input)}) end)
   end
 end
 
